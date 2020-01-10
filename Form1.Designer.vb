439d25219a05ed798ac4ddc2d023ead7f7d3af45
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2020-01-10 13:59:34 +0100

    gestion de reservas

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,22 +23,101 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(357, 166)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(252, 93)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.Size = New System.Drawing.Size(131, 46)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "prueba"
+        Me.Label1.Text = "Email:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(252, 193)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(237, 46)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Contraseña:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(289, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(200, 63)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "LOGIN"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(260, 142)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(229, 38)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(260, 242)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(229, 38)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(300, 334)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 70)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Entrar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(255, 295)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 25)
+        Me.Label4.TabIndex = 6
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(663, 193)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -48,4 +127,11 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
 End Class
---
+++ Form2.vb
@@ -0,0 +1,221 @@
+﻿
+Imports System.Globalization
+Imports MySql.Data.MySqlClient
+
+Public Class Form2
+
+    Dim con As MySqlConnection = New MySqlConnection("server=localhost;user=root;password=;database=alojamiento")
+    Dim datos As DataSet
+    Dim datos2 As DataTable
+    Dim datos3 As DataTable
+    Dim datos4 As DataTable
+    Dim adaptador As New MySqlDataAdapter
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        actualizar()
+        mostrarDNIs()
+        mostrarID_ALOJAMIENTOS()
+        ComboBox1.SelectedIndex = -1
+        ComboBox2.SelectedIndex = -1
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Try
+            Label7.Text = DataGridView1.Rows(e.RowIndex).Cells("ID").Value.ToString
+            ComboBox1.Text = DataGridView1.Rows(e.RowIndex).Cells("DNI").Value.ToString
+            TextBox2.Text = Format(DateTime.Parse(DataGridView1.Rows(e.RowIndex).Cells("FECHA_INICIO").Value.ToString()), "yyyy/MM/dd")
+            TextBox3.Text = Format(DateTime.Parse(DataGridView1.Rows(e.RowIndex).Cells("FECHA_FIN").Value.ToString()), "yyyy/MM/dd")
+            ComboBox2.Text = DataGridView1.Rows(e.RowIndex).Cells("ID_ALOJAMIENTO").Value.ToString
+        Catch
+            MsgBox("Haz click en la fila")
+        End Try
+
+
+    End Sub
+
+    Private Sub Button_BuscarPorDni(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim dni As String()
+        dni = ComboBox1.Text.Split("(")
+
+        con.Open()
+        adaptador = New MySqlDataAdapter("select * from reservas where dni='" & dni(0) & "'", con)
+        datos = New DataSet
+        adaptador.Fill(datos, "reservas")
+        DataGridView1.DataSource = datos
+        DataGridView1.DataMember = "reservas"
+        con.Close()
+
+    End Sub
+
+    Private Sub Button_Modificar(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+            If CDate(TextBox2.Text) > CDate(TextBox3.Text) Then
+                MsgBox("Error en las fecha")
+            Else
+                Dim resultado = MsgBox("¿Estas seguro de modificar esta reserva?", vbOKCancel, "CONFIRMACION")
+                If resultado = vbOK Then
+
+                    Dim dni As String()
+                    Dim id_alojamiento As String()
+                    dni = ComboBox1.Text.Split("(")
+                    id_alojamiento = ComboBox2.Text.Split("(")
+
+                    con.Open()
+                    Dim query As String = "update  reservas set ID ='" & Label7.Text & "' , DNI='" & dni(0) & "' , FECHA_INICIO='" & TextBox2.Text & "' , FECHA_FIN='" & TextBox3.Text & "' , ID_ALOJAMIENTO='" & id_alojamiento(0) & "' where ID='" & Label7.Text & "'"
+                    Dim cambio As New MySqlCommand(query, con)
+                    cambio.ExecuteNonQuery()
+                    MessageBox.Show("Reserva actualizada correctamente")
+                    con.Close()
+                    actualizar()
+                Else
+                    MsgBox("MODIFICACION CANCELADA")
+                End If
+            End If
+        Catch
+            MsgBox("ERROR DATOS INCORRECTOS")
+        End Try
+
+    End Sub
+
+    Sub actualizar()
+
+        con.Open()
+        adaptador = New MySqlDataAdapter("select * from reservas", con)
+        datos = New DataSet
+        adaptador.Fill(datos, "reservas")
+        DataGridView1.DataSource = datos
+        DataGridView1.DataMember = "reservas"
+        con.Close()
+
+    End Sub
+
+    Private Sub Button_Eliminar(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Try
+            Dim resultado = MsgBox("¿Estas seguro de borrar esta reserva?", vbOKCancel, "CONFIRMACION")
+
+            If resultado = vbOK Then
+
+                con.Open()
+                Dim query As String = "delete from reservas where ID =" & Label7.Text
+                Dim cambio As New MySqlCommand(query, con)
+                cambio.ExecuteNonQuery()
+                MessageBox.Show("Reserva eliminada correctamente")
+                con.Close()
+
+                actualizar()
+            Else
+                MsgBox("ELIMINACION CANCELADA")
+            End If
+        Catch
+            MsgBox("ERROR DATOS INCORRECTOS")
+        End Try
+
+
+    End Sub
+
+    Private Sub Button_Agregar(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Try
+
+            If CDate(TextBox2.Text) > CDate(TextBox3.Text) Then
+                MsgBox("Error en las fecha")
+            Else
+                Dim resultado = MsgBox("¿Estas seguro de insertar esta reserva?", vbOKCancel, "CONFIRMACION")
+
+                If resultado = vbOK Then
+
+                    Dim dni As String()
+                    Dim id_alojamiento As String()
+                    dni = ComboBox1.Text.Split("(")
+                    id_alojamiento = ComboBox2.Text.Split("(")
+
+                    con.Open()
+                    Dim query As String = "insert into reservas (DNI,FECHA_INICIO,FECHA_FIN,ID_ALOJAMIENTO) values ('" & dni(0) & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & id_alojamiento(0) & "')"
+                    Dim cambio As New MySqlCommand(query, con)
+                    cambio.ExecuteNonQuery()
+                    MessageBox.Show("Reserva insertada correctamente")
+                    con.Close()
+                    actualizar()
+
+                Else
+                    MsgBox("INSERCION CANCELADA")
+                End If
+            End If
+        Catch
+            MsgBox("ERROR DATOS INCORRECTOS")
+        End Try
+
+
+    End Sub
+
+    Sub mostrarDNIs()
+
+        con.Open()
+        adaptador = New MySqlDataAdapter("select DNI from usuario", con)
+        datos2 = New DataTable
+        adaptador.Fill(datos2)
+
+        adaptador = New MySqlDataAdapter("select NOMBRE from usuario", con)
+        datos3 = New DataTable
+        adaptador.Fill(datos3)
+
+        adaptador = New MySqlDataAdapter("select APELLIDOS from usuario", con)
+        datos4 = New DataTable
+        adaptador.Fill(datos4)
+
+        Dim comboSource As New Dictionary(Of String, String)()
+
+        For i = 0 To datos2.Rows.Count - 1
+
+            comboSource.Add(datos2.Rows(i).Item(0), datos2.Rows(i).Item(0) & "(" & datos3.Rows(i).Item(0) & " " & datos4.Rows(i).Item(0) & ")")
+
+        Next
+
+        ComboBox1.DataSource = New BindingSource(comboSource, Nothing)
+        ComboBox1.DisplayMember = "Value"
+        ComboBox1.ValueMember = "Key"
+
+        con.Close()
+
+    End Sub
+
+    Sub mostrarID_ALOJAMIENTOS()
+
+        con.Open()
+        adaptador = New MySqlDataAdapter("select ID from alojamientos", con)
+        datos2 = New DataTable
+        adaptador.Fill(datos2)
+
+        adaptador = New MySqlDataAdapter("select NOMBRE from alojamientos", con)
+        datos3 = New DataTable
+        adaptador.Fill(datos3)
+
+        Dim comboSource As New Dictionary(Of String, String)()
+
+        For i = 0 To datos2.Rows.Count - 1
+            comboSource.Add(datos2.Rows(i).Item(0), datos2.Rows(i).Item(0) & "(" & datos3.Rows(i).Item(0) & ")")
+        Next
+
+        ComboBox2.DataSource = New BindingSource(comboSource, Nothing)
+        ComboBox2.DisplayMember = "Value"
+        ComboBox2.ValueMember = "Key"
+        con.Close()
+
+    End Sub
+
+    Private Sub Button_Volver(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim f1 As New Form1
+        f1.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button_VerTodasLasReservas(sender As Object, e As EventArgs) Handles Button6.Click
+        actualizar()
+    End Sub
+
+
+End Class
